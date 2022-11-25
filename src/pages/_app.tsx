4245e8cea4f6bf88c5c77a5029f28dab3161ca87
nahid9764 <nahidhossain9764@gmail.com>
2022-11-26 00:50:08 +0600

    packages updated

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,15 @@
 import { wrapper } from "@store";
 import type { AppProps } from "next/app";
 import { useRouter } from "next/router";
-import { default as nProgress, default as NProgress } from "nprogress";
+import NProgress from "nprogress";
 import "nprogress/nprogress.css";
 import { useEffect } from "react";
 import "../../public/scss/app.scss";
-
 function App({ Component, pageProps }: AppProps) {
 	const router = useRouter();
 
 	useEffect(() => {
-		nProgress.configure({ showSpinner: true });
+		NProgress.configure({ showSpinner: true });
 		router.events.on("routeChangeStart", () => NProgress.start());
 		router.events.on("routeChangeComplete", () => NProgress.done());
 		router.events.on("routeChangeError", () => NProgress.done());
